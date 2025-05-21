aa3605485201b63c89d60bcd57bb1cd5e19387dd
Thomas Henrique <thomashenri@ufpa.br>
2025-05-21 18:10:56 -0300

    Feat: teste em TypeScript

---
+++ cypress/e2e/1-teste-site/2-teste-site-typescript/formulario.cy.ts
@@ -0,0 +1,81 @@
+// cypress/e2e/form.cy.ts
+
+describe('Teste no site do WC Aquino', () => {
+    beforeEach(() => {
+      cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+  
+    it('deve preencher campos de texto', () => {
+      cy.get('#formNome').type('Thomas')
+      cy.get('#formSobrenome').type('Tester')
+    })
+  
+    it('deve selecionar uma opção no combo', () => {
+      cy.get('#formEscolaridade').select('Superior')
+    })
+  
+    it('deve marcar um checkbox', () => {
+      cy.get('#formComidaPizza').check()
+    })
+  
+    it('deve marcar um radio button', () => {
+      cy.get('#formSexoMasc').check()
+    })
+  
+    it('deve validar mensagem de erro ao submeter sem preencher o nome', () => {
+      cy.get('#formCadastrar').click()
+  
+      cy.on('window:alert', (msg: string) => {
+        expect(msg).to.equal('Nome eh obrigatorio')
+      })
+    })
+  
+    it('deve validar mensagem de erro ao submeter sem preencher o sobrenome', () => {
+      cy.get('#formNome').type('Thomas')
+      cy.get('#formCadastrar').click()
+  
+      cy.on('window:alert', (msg: string) => {
+        expect(msg).to.equal('Sobrenome eh obrigatorio')
+      })
+    })
+  
+    it('deve simular prompt e cancelar no confirm', () => {
+      cy.window().then((win: Window) => {
+        cy.stub(win, 'prompt').returns('10')
+        const confirmStub = cy.stub(win, 'confirm').returns(false)
+        const alertStub = cy.stub(win, 'alert')
+  
+        cy.get('#prompt').click().then(() => {
+          expect(confirmStub.getCall(0)).to.be.calledWith('Era 10?')
+          expect(alertStub.getCall(0)).to.be.calledWith(':(')
+        })
+      })
+    })
+  
+    it('deve simular prompt, confirmar valor e verificar o alert final', () => {
+      cy.window().then((win: Window) => {
+        cy.stub(win, 'prompt').returns('42')
+  
+        const confirmStub = cy.stub(win, 'confirm').returns(true)
+        const alertStub = cy.stub(win, 'alert')
+  
+        cy.get('#prompt').click().then(() => {
+          expect(confirmStub.getCall(0)).to.be.calledWith('Era 42?')
+          expect(alertStub.getCall(0)).to.be.calledWith(':D')
+        })
+      })
+    })
+  
+    it('deve submeter o formulário', () => {
+      cy.get('#formNome').type('Thomas')
+      cy.get('#formSobrenome').type('QA')
+      cy.get('#formSexoMasc').check()
+      cy.get('#formComidaPizza').check()
+      cy.get('#formEscolaridade').select('Superior')
+      cy.get('#formEsportes').select('Futebol')
+      cy.get('#formCadastrar').click()
+  
+      cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    })
+  })
+  
